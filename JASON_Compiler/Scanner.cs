2b12ef0646fe9758f5cdb9b89906e602f6be6ffd
Ziad Khaled <ziadshraf100@gmail.com>
2024-11-14 13:40:51 +0200

    Tiny Template Code

---
+++ JASON_Compiler/Scanner.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Token_Class
+{
+    //Reserved words and operators token names
+}
+namespace Tiny_Compiler
+{
+    
+
+    public class Token
+    {
+       public string lex;
+       public Token_Class token_type;
+    }
+
+    public class Scanner
+    {
+        public List<Token> Tokens = new List<Token>();
+        Dictionary<string, Token_Class> ReservedWords = new Dictionary<string, Token_Class>();
+        Dictionary<string, Token_Class> Operators = new Dictionary<string, Token_Class>();
+
+        public Scanner()
+        {
+            //ReservedWords.Add("IF", Token_Class.If);
+            //Operators.Add(".", Token_Class.Dot); and so on
+
+
+
+
+        }
+
+    public void StartScanning(string SourceCode)
+        {
+            for(int i=0; i<SourceCode.Length;i++)
+            {
+                int j = i;
+                char CurrentChar = SourceCode[i];
+                string CurrentLexeme = CurrentChar.ToString();
+
+                if (CurrentChar == ' ' || CurrentChar == '\r' || CurrentChar == '\n')
+                    continue;
+
+                if (CurrentChar >= 'A' && CurrentChar <= 'z') //if you read a character
+                {
+                   
+                }
+
+                else if(CurrentChar >= '0' && CurrentChar <= '9')
+                {
+                   
+                }
+                else if(CurrentChar == '{')
+                {
+                   
+                }
+                else
+                {
+                   
+                }
+            }
+            
+            TINY_Compiler.TokenStream = Tokens;
+        }
+        void FindTokenClass(string Lex)
+        {
+            Token_Class TC;
+            Token Tok = new Token();
+            Tok.lex = Lex;
+            //Is it a reserved word?
+            
+
+            //Is it an identifier?
+            
+
+            //Is it a Constant?
+
+            //Is it an operator?
+
+            //Is it an undefined? (Error List)
+        }
+
+    
+
+        bool isIdentifier(string lex)
+        {
+            bool isValid=true;
+            // Check if the lex is an identifier or not.
+            
+            return isValid;
+        }
+        bool isConstant(string lex)
+        {
+            bool isValid = true;
+            // Check if the lex is a constant (Number) or not.
+
+            return isValid;
+        }
+    }
+}
